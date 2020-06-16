7ef00f42a911abfe2b45b88e1a59ad9c0462d94b
cryzed <cryzed@googlemail.com>
2020-06-16 03:59:18 +0200

    Don't make interval configurable

--- pysoundsense/gamelog.py
+++ pysoundsense/gamelog.py
@@ -8,7 +8,7 @@ _GAME_LOG_ENCODING = "CP850"
 
 
 def watch_game_log(
-    path: Path, encoding: str = _GAME_LOG_ENCODING, interval: float = 1.0
+    path: Path, encoding: str = _GAME_LOG_ENCODING
 ) -> T.Generator[str, None, None]:
     with open(path, encoding=encoding) as file:
         file.seek(0, io.SEEK_END)
@@ -16,4 +16,4 @@ def watch_game_log(
             while line := file.readline().strip():
                 yield line
 
-            time.sleep(interval)
+            time.sleep(0.01)
